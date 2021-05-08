0ed4c95640b3ea832059516a2dee6f82a373cf3e
Robin Portner <robin.portner@hotmail.com>
2021-05-08 10:53:17 +0200

    added persistence to save users

--- app/src/main/java/com/glichfalls/bmiapp/activity/CreateUserActivity.java
+++ app/src/main/java/com/glichfalls/bmiapp/activity/CreateUserActivity.java
@@ -2,13 +2,29 @@ package com.glichfalls.bmiapp.activity;
 
 import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Adapter;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Spinner;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.glichfalls.bmiapp.R;
+import com.glichfalls.bmiapp.persistence.DatabaseAdapter;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class CreateUserActivity extends MenuActivity {
+
+    private final DatabaseAdapter database = new DatabaseAdapter(this);
+
+    private Spinner spinner;
+    private List<String> employees;
+    private ArrayAdapter<String> adapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,15 +33,41 @@ public class CreateUserActivity extends MenuActivity {
         setContentView(R.layout.activity_create_user);
         Button button = findViewById(R.id.confirm_create_user_button);
         EditText input = findViewById(R.id.create_user_edit_text_name);
-        TextView username = findViewById(R.id.current_user_value);
-        username.setText(prefs.getString("name", ""));
+        spinner =  findViewById(R.id.employee_spinner);
+        database.open();
+        loadEmployees(prefs.getString("name", null));
+        System.out.println(":::" + prefs.getString("name", "none"));
         button.setOnClickListener(view -> {
             String name = input.getText().toString();
             SharedPreferences.Editor editor = prefs.edit();
             editor.putString("name", name);
             editor.apply();
-            username.setText(name);
+            database.insertEmployee(name);
+            employees.add(name);
+            spinner.setSelection(adapter.getPosition(name));
         });
-
     }
+
+    private void loadEmployees(String selected) {
+        employees = database.selectAll();
+        adapter = new ArrayAdapter<>(this, R.layout.support_simple_spinner_dropdown_item, employees);
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spinner.setAdapter(adapter);
+        spinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                String name = parent.getItemAtPosition(position).toString();
+                Toast.makeText(parent.getContext(), "Selected: " + name, Toast.LENGTH_LONG).show();
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView <?> parent) {
+            }
+        });
+        if(selected != null) {
+            System.out.println(selected);
+            spinner.setSelection(adapter.getPosition(selected));
+        }
+    }
+
 }
--- app/src/main/java/com/glichfalls/bmiapp/persistence/DatabaseAdapter.java
+++ app/src/main/java/com/glichfalls/bmiapp/persistence/DatabaseAdapter.java
@@ -1,4 +1,91 @@
 package com.glichfalls.bmiapp.persistence;
 
+import android.content.ContentValues;
+import android.content.Context;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class DatabaseAdapter {
+
+    private static final int databaseVersion = 1;
+    private static final String databaseName = "data";
+    private static final String tableName = "employee";
+
+    private final DatabaseHelper helper;
+    private SQLiteDatabase database;
+
+    public DatabaseAdapter(Context context) {
+        helper = new DatabaseHelper(context, databaseName, null, databaseVersion);
+    }
+
+    public void open() {
+        database  = helper.getWritableDatabase();
+    }
+
+    public void insertEmployee(String name) {
+        ContentValues values = new ContentValues();
+        values.put("name", name);
+        database.insert(tableName, null, values);
+    }
+
+    public String selectEmployee(int id) {
+        String[] columns = { "name" };
+        String selection = "id = ?";
+        String[] args = { Integer.toString(id) };
+        Cursor result = database.query(tableName, columns, selection, args, null, null, null);
+        return result.getString(0);
+    }
+
+    public List<String> selectAll() {
+        String[] columns = { "name" };
+        Cursor c = database.query(tableName, columns, null, null, null, null, null);
+        List<String> results = new ArrayList<>();
+        c.moveToFirst();
+        while(!c.isAfterLast()) {
+            results.add(c.getString(0));
+            c.moveToNext();
+        }
+        c.close();
+        return results;
+    }
+
+    public void updateEmployee(int id, String name) {
+        ContentValues values = new ContentValues();
+        values.put("name", name);
+        String selection = "id = ?";
+        String[] args = { Integer.toString(id) };
+        database.update(tableName, values, selection, args);
+    }
+
+    public void deleteEmployee(int id) {
+        String selection = "id = ?";
+        String[] args = { Integer.toString(id) };
+        database.delete(tableName, selection, args);
+    }
+
+    private static class DatabaseHelper extends SQLiteOpenHelper {
+
+        private static final String createTablesQuery = String.format("CREATE TABLE IF NOT EXISTS %s (id INTEGER PRIMARY KEY AUTOINCREMENT, name TEXT);", tableName);
+        private static final String dropTablesQuery = String.format("DROP TABLE IF EXISTS %s;", tableName);
+
+        public DatabaseHelper(Context context, String name, SQLiteDatabase.CursorFactory factory, int version) {
+            super(context, name, factory, version);
+        }
+
+        @Override
+        public void onCreate(SQLiteDatabase database) {
+            database.execSQL(createTablesQuery);
+        }
+
+        @Override
+        public void onUpgrade(SQLiteDatabase database, int oldVersion, int newVersion) {
+            database.execSQL(dropTablesQuery);
+            onCreate(database);
+        }
+    }
+
 }
